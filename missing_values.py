5805b8db9f7cf39a607b47dc4cffc9c10a1ad921
Winter-end <persymon.n@gmail.com>
2024-12-15 21:29:17 +0100

    test gitignore

--- missing_values.py
+++ missing_values.py
@@ -10,16 +10,11 @@ def analyze(df: pd.DataFrame) -> None:
 def identify_missing_values(df: pd.DataFrame):
     print("\nBraki w danych liczone wg. kolumn")
     missing_values = df.isnull().sum()
-    # df.isna() <==> df.isnull()   -  can be used interchangeably
     print(missing_values[missing_values > 0])
-    # df.isnull().sum().sum() - all the missing values
 
     print("\nBraki w danych liczone wg. obiektów")
     missing_objects = df.isnull().sum(axis = 1)
     print(missing_objects[missing_objects > 0])
-    
-    # print("\nMissing Values Count by Objects (Grouped by Street):")
-    # print(df[df.isnull().sum(axis = 1) > 0].groupby("Street").size())
 
 def visualize_missing_values(df: pd.DataFrame):
     plt.figure(figsize=(12, 8))
